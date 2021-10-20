33216bd46bdd7c4a1e6a06e538f73ecd155c0d7f
sameer <here.i.am.sam@gmail.com>
2021-10-20 21:08:30 +0530

    Create User entity class to represent the users taht will be returned by our custom UserDetailsService

---
+++ src/main/java/app/auth/User.java
@@ -0,0 +1,64 @@
+package app.auth;
+
+import java.util.Collection;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class User implements UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	private final String username;
+	private final String password;
+	private final Set<? extends GrantedAuthority> grantedAuthorities;
+	private final boolean isAccountNonExpired;
+	private final boolean isAccountNonLocked;
+	private final boolean isCredentialsNonExpired;
+	private final boolean isEnabled;
+	public User(String username, String password,
+			Set<? extends GrantedAuthority> grantedAuthorities,
+			boolean isAccountNonExpired, boolean isAccountNonLocked,
+			boolean isCredentialsNonExpired, boolean isEnabled) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.grantedAuthorities = grantedAuthorities;
+		this.isAccountNonExpired = isAccountNonExpired;
+		this.isAccountNonLocked = isAccountNonLocked;
+		this.isCredentialsNonExpired = isCredentialsNonExpired;
+		this.isEnabled = isEnabled;
+	}
+	@Override
+	public String getUsername() {
+		return username;
+	}
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return isAccountNonExpired;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		return isAccountNonLocked;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return isCredentialsNonExpired;
+	}
+	@Override
+	public boolean isEnabled() {
+		return isEnabled;
+	}
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return grantedAuthorities;
+	}
+
+}
