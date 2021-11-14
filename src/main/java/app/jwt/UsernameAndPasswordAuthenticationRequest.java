367120905749ec2393f7deee13c28a84d2f6c906
sameer <here.i.am.sam@gmail.com>
2021-11-14 20:37:06 +0530

    Create serializing class for Username and Password
    The serializing class will be used by Jackson plugin to extract the username/password
    from the HttpServletRequest and map to an object of the serializing class

---
+++ src/main/java/app/jwt/UsernameAndPasswordAuthenticationRequest.java
@@ -0,0 +1,26 @@
+package app.jwt;
+
+public class UsernameAndPasswordAuthenticationRequest {
+
+	private String username, password;
+
+	public UsernameAndPasswordAuthenticationRequest() {
+		super();
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
